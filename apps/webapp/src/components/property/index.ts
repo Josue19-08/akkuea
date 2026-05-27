98ea2c405c029fd6ef1d90888d676d92d07756d2
clintjeff2 <clintjeff2@gmail.com>
2026-05-27 18:51:41 +0100

    docs: add comprehensive 3D capture and processing guide for property owners

---
+++ apps/webapp/src/components/property/index.ts
@@ -0,0 +1,5 @@
+import dynamic from "next/dynamic";
+export { PropertyDetail } from "./PropertyDetail";
+export { PropertyViewer3D } from "./PropertyViewer3D";
+
+export type { PropertyViewer3DProps } from "./PropertyViewer3D";
